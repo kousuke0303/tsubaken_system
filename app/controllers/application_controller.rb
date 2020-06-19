543f3c8956ec87b68cb819c6c846a8ae8580c21d
EC2 Default User <ec2-user@ip-172-31-10-106.ap-northeast-1.compute.internal>
2020-06-19 09:12:30 +0000

    matter_crud

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_submanager_public_uid
+  helper_method :current_matter
   
   # login画面等のデザインformat指定
   def non_approval_layout
@@ -24,6 +25,12 @@ class ApplicationController < ActionController::Base
       flash[:alert] = "アクセス権限がありません"
       redirect_to root_path
     end
+  end
+  
+  # ---------------MATTER関係----------------------------------
+  
+  def current_matter
+    Matter.find_by(matter_uid: params[:id])
   end
   
 
--- app/models/matter.rb
+++ app/models/matter.rb
@@ -1,9 +1,9 @@
 class Matter < ApplicationRecord
-  has_many :matter_managers
+  has_many :matter_managers, dependent: :destroy
   has_many :managers, through: :matter_managers
   
-  has_many :clients
-  accepts_nested_attributes_for :clients
+  has_many :clients, dependent: :destroy
+  accepts_nested_attributes_for :clients, allow_destroy: true
   
   def to_param
     matter_uid ? matter_uid : super()
