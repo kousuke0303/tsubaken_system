ce3b2a5a4105018323db28282e2cb1df0b21f853
EC2 Default User <ec2-user@ip-172-31-10-106.ap-northeast-1.compute.internal>
2020-06-22 06:01:09 +0000

    連結コード表示

--- app/controllers/user/matters_controller.rb
+++ app/controllers/user/matters_controller.rb
@@ -3,6 +3,7 @@ class User::MattersController < ApplicationController
   def matter_connect
     if @matter = Matter.find_by(matter_uid: params[:connencted_id])
       @matter.matter_users.create(user_id: current_user.id)
+      connected_manager(@matter)
       redirect_to top_user_url(current_user)
     else
       flash[:alert] = "正しい連結コードを入力してください"
