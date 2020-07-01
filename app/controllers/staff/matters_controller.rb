c4976ed448f9ddfe53cd00c3d7b141127122f99b
EC2 Default User <ec2-user@ip-172-31-10-106.ap-northeast-1.compute.internal>
2020-07-01 13:58:01 +0000

    comp/staff_tasks

--- app/controllers/staff/matters_controller.rb
+++ app/controllers/staff/matters_controller.rb
@@ -8,6 +8,26 @@ class Staff::MattersController < ApplicationController
   end
   
   def show
+    matter_task_type
   end
+  
+  def move_task
+    task = Task.find(remove_str(params[:task]))
+    task.update(status: params[:status], row_order: roworder_params)
+    matter_task_type
+    respond_to do |format|
+      format.js
+    end
+  end
+  
+  private
+    # 文字列から数字のみ取り出す
+    def remove_str(str)
+      str.gsub(/[^\d]/, "").to_i
+    end
+    
+    def roworder_params
+      (params[:item_index].to_i * 100 ) - 1
+    end
   
 end
