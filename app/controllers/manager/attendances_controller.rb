8914c1057b72cbfe5d89e1e94a74a9585ddcd746
Hiroki37 <hiroki10037@gmail.com>
2020-07-09 00:44:17 +0900

    submanagerのプルダウン選んだときに表示できるようになった

--- app/controllers/manager/attendances_controller.rb
+++ app/controllers/manager/attendances_controller.rb
@@ -1,5 +1,20 @@
 class Manager::AttendancesController < ApplicationController
   
   def index
+    @manager = Manager.find_by(public_uid: params[:id])
+    unless params[:employee_type].nil?
+      @employee_type = params[:employee_type]
+      @split_employee = @employee_type.split('#')
+      @employee_name = @split_employee[0]
+      @employee_id = @split_employee[1]
+      p @employee_name
+      p @employee_id
+      @submanagers = Submanagers::Attendance.where(submanager_id: @employee_id)
+      # @staff = Staffs::Attendance.where(staff_id: @employee_id)
+    end
+    respond_to do |format|
+      format.js
+      format.any
+    end
   end
 end
--- app/models/submanagers/attendance.rb
+++ app/models/submanagers/attendance.rb
@@ -1,3 +1,4 @@
 class Submanagers::Attendance < ApplicationRecord
   belongs_to :submanager
+  belongs_to :matter
 end
