83f396af1339f11be11798a91890c5a4add83021
EC2 Default User <ec2-user@ip-172-31-10-106.ap-northeast-1.compute.internal>
2020-07-15 08:46:21 +0000

    fix/seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   before_save { self.email = email.downcase }
-  before_save { self.phone = phone.downcase }
   
   # validation ############################################
   
