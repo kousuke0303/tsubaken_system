d352af714ce4d304811cef6f0737291c85677cc6
Hiroki37 <hiroki10037@gmail.com>
2020-07-20 13:28:02 +0900

    文字列で月を判定するよう修正途中

--- app/controllers/manager/attendances_controller.rb
+++ app/controllers/manager/attendances_controller.rb
@@ -8,10 +8,8 @@ class Manager::AttendancesController < ApplicationController
       @split_employee = @employee_type.split('#')
       @employee_name = @split_employee[0]
       @employee_id = @split_employee[1]
-      p @employee_name
-      p @employee_id
-      @submanagers = Submanagers::Attendance.where(submanager_id: @employee_id)
-      @staff = Staffs::Attendance.where(staff_id: @employee_id)
+      @submanagers = Submanagers::Attendance.where(submanager_id: @employee_id).where(date: @first_day).worked_on.strftime('%m')
+      @staffs = Staffs::Attendance.where(staff_id: @employee_id)
     end
     respond_to do |format|
       format.js
