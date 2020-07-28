ec59337df6b7c526b9e71c431ae8c58cee9b5bd8
EC2 Default User <ec2-user@ip-172-31-10-106.ap-northeast-1.compute.internal>
2020-07-28 08:37:24 +0000

    fix/address_disp

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,148 @@
+require 'net/https'
+require 'uri'
+
+class AddressesController < ApplicationController
+  
+  def prefecture_index
+    uri = URI.parse('https://addresssearch.herokuapp.com/api/v1/posts/prefecture')
+    http = Net::HTTP.new(uri.host, uri.port)
+    
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    
+    req = Net::HTTP::Get.new(uri.path)
+    res = http.request(req)
+    
+    begin
+      case res
+      when Net::HTTPSuccess
+        # responseのbody要素をJSON形式で解釈し、hashに変換
+        result = JSON.parse(res.body)
+        @prefectures = result["data"]
+        # 表示用の変数に結果を格納
+        respond_to do |format|
+          format.js
+        end
+      end
+    rescue IOError => e
+      @message = "e.message"
+    rescue TimeoutError => e
+      @message = "e.message"
+    rescue JSON::ParserError => e
+      @message = "e.message"
+    rescue => e
+      @message = "e.message"
+    end
+  end
+  
+  def city_index
+    @select_prefecture_name = params[:ken_name]
+    select_prefecture = params[:ken_id]
+    target_city_params = URI.encode_www_form({ken_id: select_prefecture})
+    uri = URI.parse("https://addresssearch.herokuapp.com/api/v1/posts/ken_select?#{target_city_params}")
+    http = Net::HTTP.new(uri.host, uri.port)
+    
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    
+    req = Net::HTTP::Get.new(uri.path + "?" + uri.query)
+    res = http.request(req)
+    
+    begin
+      case res
+      when Net::HTTPSuccess
+        # responseのbody要素をJSON形式で解釈し、hashに変換
+        result = JSON.parse(res.body)
+        @cities = result["data"]
+        # 表示用の変数に結果を格納
+        respond_to do |format|
+          format.js
+        end
+      end
+    rescue IOError => e
+      @message = "e.message"
+    rescue TimeoutError => e
+      @message = "e.message"
+    rescue JSON::ParserError => e
+      @message = "e.message"
+    rescue => e
+      @message = "e.message"
+    end
+    
+  end
+  
+  def town_index
+    @select_prefecture_name = params[:ken_name]
+    @select_city_name = params[:city_name]
+    select_city = params[:city_id]
+    target_town_params = URI.encode_www_form({city_id: select_city})
+    uri = URI.parse("https://addresssearch.herokuapp.com/api/v1/posts/city_select?#{target_town_params}")
+    http = Net::HTTP.new(uri.host, uri.port)
+    
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    
+    req = Net::HTTP::Get.new(uri.path + "?" + uri.query)
+    res = http.request(req)
+    
+    begin
+      case res
+      when Net::HTTPSuccess
+        # responseのbody要素をJSON形式で解釈し、hashに変換
+        result = JSON.parse(res.body)
+        @towns = result["data"]
+        # 表示用の変数に結果を格納
+        respond_to do |format|
+          format.js
+        end
+      end
+    rescue IOError => e
+      @message = "e.message"
+    rescue TimeoutError => e
+      @message = "e.message"
+    rescue JSON::ParserError => e
+      @message = "e.message"
+    rescue => e
+      @message = "e.message"
+    end
+  end
+  
+  def block_index
+    @select_prefecture_name = params[:ken_name]
+    @select_city_name = params[:city_name]
+    @select_town_name = params[:town_name]
+    select_town = params[:town_id]
+    target_block_params = URI.encode_www_form({town_id: select_town})
+    uri = URI.parse("https://addresssearch.herokuapp.com/api/v1/posts/town_select?#{target_block_params}")
+    http = Net::HTTP.new(uri.host, uri.port)
+    
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    
+    req = Net::HTTP::Get.new(uri.path + "?" + uri.query)
+    res = http.request(req)
+    
+    begin
+      case res
+      when Net::HTTPSuccess
+        # responseのbody要素をJSON形式で解釈し、hashに変換
+        result = JSON.parse(res.body)
+        @blocks = result["data"]
+        debugger
+        # 表示用の変数に結果を格納
+        respond_to do |format|
+          format.js
+        end
+      end
+    rescue IOError => e
+      @message = "e.message"
+    rescue TimeoutError => e
+      @message = "e.message"
+    rescue JSON::ParserError => e
+      @message = "e.message"
+    rescue => e
+      @message = "e.message"
+    end
+  end
+  
+end
