db66d1886e74aa20930e50ed264d85bfabdbb4e2
kousuke0303 <kousuke0303@gmail.com>
2020-09-26 00:43:13 +0900

    fix

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -2,15 +2,7 @@ class Supplier < ApplicationRecord
   before_save { self.mail = mail.downcase }
   before_save { self.phone = phone.tr('０-９ａ-ｚＡ-Ｚ','0-9a-zA-Z') }
   before_save { self.fax = fax.tr('０-９ａ-ｚＡ-Ｚ','0-9a-zA-Z') }
-  
-  # validation ############################################
-  
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :mail, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  
-  
-  # アソシエーション ######################################
-  belongs_to :manager
+  validates :mail, length: { maximum: 254 }, format: { with: VALID_EMAIL_REGEX }
   
   # scope #################################################
   scope :for_order_count, -> { order(count: "DESC") } 
