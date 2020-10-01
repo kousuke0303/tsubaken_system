48ae2fd7b0a9f598ef8ae6d407330f6d39018134
kousuke0303 <kousukeee0303@gmail.com>
2020-10-01 12:14:17 +0900

    fix

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Admins::RegistrationsController < Devise::RegistrationsController
   before_action :authenticate_admin!
   before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+  # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/edit
   # def edit
@@ -11,9 +11,16 @@ class Admins::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    @admin = Admin.find(current_admin.id)
+    if @admin.update(admin_params)
+      sign_in(@admin, :bypass => true)
+      flash[:success] = "アカウント情報を更新しました"
+      redirect_to top_admin_url(@admin)
+    else
+      render :edit
+    end
+  end
 
   private
    def admin_params
@@ -27,14 +34,14 @@ class Admins::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :employee_id])
   end
 
-  def after_update_path_for(resource)
-    admin_top_path
-  end
+  # def after_update_path_for(resource)
+  #   admin_top_path
+  # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :employee_id])
-  end
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:name, :employee_id])
+  # end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
