dbe9edfe0b66f2cb4a8fdaedf8f4f883ef9da140
kousuke0303 <kousukeee0303@gmail.com>
2020-10-01 13:32:08 +0900

    fix

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Admins::RegistrationsController < Devise::RegistrationsController
   before_action :authenticate_admin!
   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/edit
   # def edit
@@ -35,13 +35,13 @@ class Admins::RegistrationsController < Devise::RegistrationsController
   end
 
   # def after_update_path_for(resource)
-  #   admin_top_path
+  #   admin_top_path(resource)
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:name, :employee_id])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :employee_id])
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
