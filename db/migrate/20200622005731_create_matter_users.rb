1f0ab3b11508d38adadf79daeb008207e610d64a
kousuke0303 <kousukeee0303@gmail.com>
2020-10-06 14:48:50 +0900

    fix

--- db/migrate/20200622005731_create_matter_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateMatterUsers < ActiveRecord::Migration[5.1]
-  def change
-    create_table :matter_users do |t|
-      t.references :matter, index: true, foreign_key: true
-      t.references :user, index: true, foreign_key: true
-      t.timestamps
-    end
-  end
-end
