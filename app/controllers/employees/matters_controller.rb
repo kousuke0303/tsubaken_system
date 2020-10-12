9a302bb922804a85dbf276e2f4831aa22ba0bed9
kousuke0303 <kousuke0303@gmail.com>
2020-10-12 21:53:13 +0900

    fix

--- app/controllers/employees/matters_controller.rb
+++ app/controllers/employees/matters_controller.rb
@@ -1,13 +1,23 @@
 class Employees::MattersController < ApplicationController
   before_action :authenticate_employee!
+  before_action :set_matter, only: [:show, :edit, :update, :destroy]
 
   def index
   end
   
   def new
+    @matter = Matter.new
+    @clients = Client.all
   end
 
   def create
+    @matter = Matter.new(matter_params)
+    if @matter.save
+      flash[:success] = "案件を作成しました"
+      redirect_to employees_matter_url(@matter)
+    else
+      render :new
+    end
   end
 
   def show
@@ -17,9 +27,17 @@ class Employees::MattersController < ApplicationController
   end
 
   def update
+    if @matter.update(matter_params)
+      flash[:success] = "案件情報を更新しました"
+      redirect_to employees_matter_url(@matter)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @matter.destroy ? flash[:success] = "案件を削除しました" : flash[:alert] = "案件を削除できませんでした"
+    redirect_to employees_matters_url
   end
 
   private
@@ -28,6 +46,6 @@ class Employees::MattersController < ApplicationController
     end
 
     def matter_params
-      params.require(:matter).permit(:name)
+      params.require(:matter).permit(:title, :client_id, :zip_code, :actual_spot, :scheduled_started_on, :scheduled_finished_on)
     end
 end
