dbaaaa27ab739c55a1b14043d1f3210d869ccf9b
tomoyakun1221 <tomoyakun.1221@gmail.com>
2020-10-13 12:18:35 +0000

    顧客のログイン機能

--- app/controllers/clients/sessions_controller.rb
+++ app/controllers/clients/sessions_controller.rb
@@ -1,27 +1,35 @@
 # frozen_string_literal: true
 
 class Clients::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    client = Client.find_by(login_id: params[:session][:login_id])
+    if client && client.authenticate(params[:session][:password])
+      log_in client
+      params[:session][:remember_me] == "1" ? remember(client) : forget(client)
+      redirect_back_or client
+    else
+      flash[:danger] = "認証に失敗しました。"
+      render :new
+    end
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+  end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  end
 end
