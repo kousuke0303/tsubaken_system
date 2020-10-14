40fc7de02593bb4ef91c2e3018e8d187f0a14ddb
kousuke0303 <kousukeee0303@gmail.com>
2020-10-14 17:11:26 +0900

    fix

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,6 +2,7 @@ class Attendance < ApplicationRecord
   validates :worked_on, presence: true
   validate :started_is_with_finished
   validate :finished_is_after_started
+  validate :require_worker
 
   belongs_to :manager, optional: true
   belongs_to :staff, optional: true
@@ -15,5 +16,9 @@ class Attendance < ApplicationRecord
   # 出勤以前の退勤は無効
   def finished_is_after_started
     errors.add(:finished_at, "は出勤時間以降に入力してください") if started_at.present? && finished_at.present? && finished_at <= started_at
+  end
+
+  def require_worker
+    errors.add(:base, "勤務者を入力してください") if manager_id.blank? && staff_id.blank? && external_staff_id.blank?
   end
 end
