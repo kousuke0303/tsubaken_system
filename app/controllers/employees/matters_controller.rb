b5e7a9beb3d4c99e5eab61d5ebe8c507c0a1df5d
kousuke0303 <kousukeee0303@gmail.com>
2020-10-15 14:27:10 +0900

    fix

--- app/controllers/employees/matters_controller.rb
+++ app/controllers/employees/matters_controller.rb
@@ -74,6 +74,6 @@ class Employees::MattersController < ApplicationController
 
     def matter_params
       params.require(:matter).permit(:title, :client_id, :zip_code, :actual_spot, :scheduled_started_on, :scheduled_finished_on,
-                                     status, { :manager_ids=> [] }, { :staff_ids=> [] }, { :supplier_ids=> [] })
+                                     :status, { :manager_ids=> [] }, { :staff_ids=> [] }, { :supplier_ids=> [] })
     end
 end
