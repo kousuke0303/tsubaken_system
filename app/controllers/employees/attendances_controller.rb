3c38bb344bacda99a93695523fca75ba46c6db2c
kousuke0303 <kousuke0303@gmail.com>
2020-10-23 21:55:30 +0900

    fix

--- app/controllers/employees/attendances_controller.rb
+++ app/controllers/employees/attendances_controller.rb
@@ -7,10 +7,10 @@ class Employees::AttendancesController < ApplicationController
     # 対象日を定義
     params[:day] && params[:day].present? ? @day = params[:day].to_date : @day = Date.current
     # 勤怠モデルと、マネージャー・スタッフ・外部スタッフ(所属の外注先を含めて)を事前に読み込む
-    attendances = Attendance.where(worked_on: @day).includes(:manager).includes(:staff).includes(external_staff: :supplier)
-    @manager_attendances = attendances.where(manager_id: !nil)
-    @staff_attendances = attendances.where(staff_id: !nil)
-    @external_staff_attendances = attendances.where(external_staff_id: !nil)
+    attendances = Attendance.where(worked_on: @day).where.not(started_at: nil).includes(:manager).includes(:staff).includes(external_staff: :supplier)
+    @manager_attendances = attendances.where.not(manager_id: nil)
+    @staff_attendances = attendances.where.not(staff_id: nil)
+    @external_staff_attendances = attendances.where.not(external_staff_id: nil)
   end
 
   # 従業員別の月毎の勤怠表示ページ
