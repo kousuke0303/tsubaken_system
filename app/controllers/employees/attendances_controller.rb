1cbc6c35adfb05f071129f3b179379453a691d62
kousuke0303 <kousuke0303@gmail.com>
2020-10-24 09:54:59 +0900

    fix

--- app/controllers/employees/attendances_controller.rb
+++ app/controllers/employees/attendances_controller.rb
@@ -1,6 +1,7 @@
 class Employees::AttendancesController < ApplicationController
   before_action :authenticate_admin_or_manager!
   before_action :set_one_month, only: :individual
+  before_action :set_latest_30_year, only: :individual
 
   # 日別勤怠表示ページ
   def daily
@@ -15,6 +16,22 @@ class Employees::AttendancesController < ApplicationController
 
   # 従業員別の月毎の勤怠表示ページ
   def individual
+    @years_hash = Hash.new
+    latest_year = Date.current.year
+    [*latest_year - 30..latest_year].each { |year| @years_hash.store("#{year}年", year) }
+    @years_hash = @years_hash.sort.reverse.to_h
+    @managers = Manager.all
+    @staffs = Staff.all
+    @external_staffs = ExternalStaff.all
     @attendances = Attendance.where(manager_id: !nil)
   end
+
+  private
+    # 直近30年をhashに(フォーム用)
+    def set_latest_30_year
+      @years_hash = Hash.new
+      latest_year = @first_day.year
+      [*latest_year - 30..latest_year].each { |year| @years_hash.store("#{year}年", year) }
+      @years_hash = @years_hash.sort.reverse.to_h
+    end
 end
