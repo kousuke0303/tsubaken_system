2655f9631559e9ba0fefcfb0cbf71f415ccc3c5c
EC2 Default User <ec2-user@ip-172-31-18-228.ap-northeast-1.compute.internal>
2020-10-24 00:49:37 +0000

    clientのapi実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post "sign_in", to: "sessions#create"
+      namespace :employees do
+        resources :clients
+      end
     end
   end
   
