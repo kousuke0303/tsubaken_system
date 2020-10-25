74b16504e1098db378d6ee8cd605662e294528af
tomoyakun1221 <tomoyakun.1221@gmail.com>
2020-10-25 10:16:54 +0000

    タスク実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :matter
   
+  enum status: {matter_task: 0, progress_tasks: 1, finished_tasks: 2}
+  
   scope :are_matter_tasks, -> { where(status: "matter_tasks").order(:row_order) }
   scope :are_progress_tasks, -> { where(status: "progress_tasks").order(:row_order) }
   scope :are_finished_tasks, -> { where(status: "finished_tasks").order(:row_order) }
