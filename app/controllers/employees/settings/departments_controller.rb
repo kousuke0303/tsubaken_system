7923b520d9d47a54553df310b68e1282745b45dd
kousuke0303 <kousukeee0303@gmail.com>
2020-10-26 15:48:29 +0900

    fix

--- app/controllers/employees/settings/departments_controller.rb
+++ app/controllers/employees/settings/departments_controller.rb
@@ -32,22 +32,14 @@ class Employees::Settings::DepartmentsController < ApplicationController
   end
 
   def destroy
-    # 無所属は削除できないように設定
-    if @department.id == 1
-      flash[:alert] = "無所属は削除できません"
-      exit
+    if @department.id != 1
+      @department.managers.update_all(department_id: 1)
+      @department.staffs.update_all(department_id: 1)
+      @department.delete
+      flash[:success] = "部署を削除しました"
+    else
+      flash[:notice] = "部署を削除できませんでした"
     end
-    @manager_dept_ids = Manager.pluck(:department_id)
-    @staff_dept_ids = Staff.pluck(:department_id)
-    # 部署削除時、その部署に所属のマネージャーを無所属にupdate
-    if @manager_dept_ids.any?{|i| i == @department.id}
-      Manager.where('department_id = ?', @department.id).update_all(:department_id => 1)
-    end
-    # 部署削除時、その部署に所属のスタッフを無所属にupdate
-    if @staff_dept_ids.any?{|i| i == @department.id}
-      Staff.where('department_id = ?', @department.id).update_all(:department_id => 1)
-    end
-    @department.destroy ? flash[:success] = "部署を削除しました" : flash[:alert] = "部署を削除できませんでした"
     redirect_to employees_settings_departments_url
   end
 
--- app/models/department.rb
+++ app/models/department.rb
@@ -1,4 +1,5 @@
 class Department < ApplicationRecord
+  before_destroy :prevent_delete_first
   validates :name, presence: true, length: { maximum: 30 }
   validate :restrict_edit_first
   
@@ -8,5 +9,13 @@ class Department < ApplicationRecord
   # 無所属部署は編集不可
   def restrict_edit_first
     errors.add(:base, "無所属部署は編集できません") if id == 1 && name != "無所属"
+  end
+
+  # 無所属部署は削除不可
+  def prevent_delete_first
+    if id == 1
+      errors.add(:base, "無所属部署は削除できません")
+      throw :abort
+    end
   end
 end
