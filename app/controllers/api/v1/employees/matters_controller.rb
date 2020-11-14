2e438d1bb8b393d716f5748c88cac7c1b07ac691
kousuke0303 <kousuke0303@gmail.com>
2020-11-14 16:36:06 +0900

    fix

--- app/controllers/api/v1/employees/matters_controller.rb
+++ app/controllers/api/v1/employees/matters_controller.rb
@@ -1,9 +1,25 @@
 class Api::V1::Employees::MattersController < Api::V1::ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :check_token_and_key_to_api
+  before_action :set_matter, only: [:update, :show, :destroy]
+
+  def create
+    matter = Matter.new(matter_params)
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 
   private
     def set_matter
       @matter = Matter.find(params[:id])
+    end
+
+    def matter_params
+      params.permit(:id, :title, :actual_spot, :zip_code, :status, :content, :scheduled_started_on, :started_on, :scheduled_finished_on,
+                    :finished_on, :maintenanced_on, :client_id, { manager_ids: [] }, { staff_ids: [] }, { supplier_ids: [] })
     end
 end
