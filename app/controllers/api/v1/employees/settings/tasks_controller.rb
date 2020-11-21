c2dea889eee873f931d8f89705f5ef31b44a89a4
白井　徹 <zabiisann@gmail.com>
2020-11-21 09:50:48 +0900

    デフォルトタスクのapi作成

---
+++ app/controllers/api/v1/employees/settings/tasks_controller.rb
@@ -0,0 +1,40 @@
+class Api::V1::Employees::Settings::TasksController < Api::V1::ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :check_token_and_key_to_api
+  before_action :set_task, only: [:update, :destroy]
+
+  def create
+    sort_order = Task.are_default.length
+    @default_task = Task.new(default_task_params.merge(status: 0, sort_order: sort_order))
+    if @default_task.save
+      render json: industry, serializer: TaskSerializer
+    else
+      render json: { status: "false", message: industry.errors.full_messages }
+    end
+  end
+
+  def update
+    if @default_task.update(default_task_params)
+      render json: @default_task, serializer: TaskSerializer
+    else
+      render json: { status: "false", message: @default_task.errors.full_messages }
+    end
+  end
+
+  def destroy
+    if @default_task.destroy
+      render json: @default_task, serializer: TaskSerializer
+    else
+      render json: { status: "false", message: @default_task.errors.full_messages }
+    end
+  end
+
+  private
+    def default_task_params
+      params.require(:task).permit(:title, :content)
+    end
+
+    def set_task
+      @default_task= Task.find(params[:id])
+    end
+end
