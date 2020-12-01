6ab096f06b6957a4486bf39ac558824d81424581
kousuke0303 <kousukeee0303@gmail.com>
2020-12-01 17:40:51 +0900

    fix

--- app/controllers/employees/estimate_matters_controller.rb
+++ app/controllers/employees/estimate_matters_controller.rb
@@ -1,5 +1,6 @@
 class Employees::EstimateMattersController < ApplicationController
   before_action :authenticate_employee!
+  before_action :set_estimate_matter, only: [:show, :edit, :update, :destroy]
 
   def index
     @estimate_matters = EstimateMatter.all
@@ -7,24 +8,49 @@ class Employees::EstimateMattersController < ApplicationController
 
   def new
     @estimate_matter = EstimateMatter.new
+    @clients = Client.all
   end
 
   def create
+    @estimate_matter = EstimateMatter.new(estimate_matter_params)
+    if @estimate_matter.save
+      flash[:success] = "見積案件を作成しました。"
+      redirect_to employees_estimate_matters_url(@estimate_matter)
+    else
+      respond_to do |format|
+        format.js
+      end
+    end
+  end
+
+  def show
   end
 
   def edit
   end
 
   def update
+    if @estimate_matter.update(estimate_matter_params)
+      flash[:success] = "見積案件を更新しました。"
+      redirect_to employees_estimate_matters_url(@estimate_matter)
+    else
+      respond_to do |format|
+        format.js
+      end
+    end
   end
 
   def destroy
+    @estimate_matter.destroy ? flash[:success] = "見積案件を削除しました。" : flash[:alert] = "見積案件を削除できませんでした。"
+    redirect_to employees_estimate_matters_url
   end
 
   private
     def set_estimate_matter
+      @estimate_matter = EstimateMatter.find(params[:id])
     end
 
     def estimate_matter_params
+      params.require(:estimate_matter).permit(:title, :client_id, :status)
     end
 end
