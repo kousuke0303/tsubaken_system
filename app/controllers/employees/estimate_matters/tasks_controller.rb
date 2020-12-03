ea3a689459ebcb7678749a47751b8bbd0f2e0bf7
kousuke0303 <kousukeee0303@gmail.com>
2020-12-03 10:05:11 +0900

    fin/add_task_to_estimate

--- app/controllers/employees/estimate_matters/tasks_controller.rb
+++ app/controllers/employees/estimate_matters/tasks_controller.rb
@@ -3,6 +3,38 @@ class Employees::EstimateMatters::TasksController < ApplicationController
   before_action :set_estimate_matter
   before_action :set_task, only: [:edit, :update, :destroy]
 
+  def move
+    new_status = convert_to_status_num(params[:status]).to_i
+    @default_tasks = Task.are_default
+    # 移動先がデフォルトタスクでない場合のみ処理
+    unless new_status == 0
+      task = Task.find(params[:task])
+      sort_order = params[:item_index].to_i
+      # 同ステータス内で上に移動時
+      if task.status_before_type_cast == new_status && task.sort_order < sort_order
+        # 対象タスクより、優先順位が上の全タスクのsort_orderを-1
+        Task.decrement_sort_order(@estimate_matter, new_status, sort_order)
+        task.update(moved_on: Time.current, sort_order: sort_order)
+      # ステータス移動、又は下に移動時
+      else
+        # 対象タスクより、優先順位が下の全タスクのsort_orderを+1
+        Task.increment_sort_order(@estimate_matter, new_status, sort_order)
+        if task.default?
+          # デフォルトタスクからコピー
+          @estimate_matter.tasks.create(title: task.title, content: task.content, status: new_status, default_task_id: task.id, sort_order: sort_order)
+          Task.count_default_tasks(@default_tasks)
+        else
+          # タスク移動
+          task.update(status: new_status, moved_on: Time.current, before_status: task.status, sort_order: sort_order)
+        end
+      end
+    end
+    set_classified_tasks(@estimate_matter)
+    respond_to do |format|
+      format.js
+    end
+  end
+
   def create
     # 作成前に進行中タスクのsort_orderを更新
     relevant_tasks = @estimate_matter.tasks.are_relevant
