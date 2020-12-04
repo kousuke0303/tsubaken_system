c45007b8083b7b0f3e877c4ed0c9c8109210bcaa
kousuke0303 <kousukeee0303@gmail.com>
2020-12-04 15:21:03 +0900

    fix

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -6,5 +6,7 @@ class Estimate < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 30 }
 
-  scope :with_categories, -> { left_joins(:categories).select("estimates.*", "categories.*") }
+  scope :with_categories, -> { left_joins(:categories).select("estimates.*",
+                                                              "categories.*",
+                                                              "estimates.id AS id") }
 end
