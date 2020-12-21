f53dadb48a108d188b38bcf3cae04059244ddfbb
kousuke0303 <kousukeee0303@gmail.com>
2020-12-21 11:06:43 +0900

    fin/practitioner_validation

--- app/models/sales_status.rb
+++ app/models/sales_status.rb
@@ -6,7 +6,17 @@ class SalesStatus < ApplicationRecord
   validates :status, presence: true
   validates :conducted_on, presence: true
   validates :note, length: { maximum: 300 }
+  validate :require_only_practitioner
 
   enum status: { other: 0, appointment: 1, inspection: 2, calculation: 3, immediately: 4, contract: 5, contemplation_month: 6,
                  contemplation_year: 7, lost: 8, lost_by_other: 9, chasing: 10, waiting_insurance: 11, consideration: 12 }
+
+  # 一名の実施者を必須にする
+  def require_only_practitioner
+    if staff_id.nil? && external_staff_id.nil?
+      errors.add(:base, "実施者を入力してください")
+    elsif staff_id.present? && external_staff_id.present?
+      errors.add(:base, "実施者は一名のみ入力可能です")
+    end
+  end
 end
