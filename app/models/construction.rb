58b754dc6627696b26be58df05da370df8351dbe
kousuke0303 <kousukeee0303@gmail.com>
2021-01-03 16:57:18 +0900

    rm/scss_js

--- app/models/construction.rb
+++ app/models/construction.rb
@@ -15,6 +15,7 @@ class Construction < ApplicationRecord
   # 引数に入れた見積案件の持つ工事のみを返す
   scope :of_estimate_matter, -> (estimate_matter_id) { left_joins(category: :estimate).where(estimates: { estimate_matter_id: estimate_matter_id }) }
 
+  # 数量と単価があれば、その合計値をtotalカラムに保存
   def calc_total    
     self.total = price * amount if price.present? && amount.present?
   end
