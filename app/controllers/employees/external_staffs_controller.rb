f0f141f5ca8802e697d50131aca62a912b93abf5
kousuke0303 <kousukeee0303@gmail.com>
2021-01-06 17:59:55 +0900

    fix/ex_staff_login_id

--- app/controllers/employees/external_staffs_controller.rb
+++ app/controllers/employees/external_staffs_controller.rb
@@ -1,17 +1,17 @@
 class Employees::ExternalStaffsController < ApplicationController
   before_action :authenticate_admin_or_manager!
+  before_action :set_suppliers, only: [:new, :edit]
   before_action :set_external_staff, only: [:show, :edit, :update, :destroy]
-  before_action :set_supplier, only: [:new, :create, :show, :edit, :update, :destroy]
 
   def new
-    @external_staff = @supplier.external_staffs.new
+    @external_staff = ExternalStaff.new
   end
 
   def create
-    @external_staff = @supplier.external_staffs.new(external_staff_params.merge(password: "password", password_confirmation: "password"))
+    @external_staff = ExternalStaff.new(external_staff_params.merge(password: "password", password_confirmation: "password"))
     if @external_staff.save
       flash[:success] = "外部Staffを作成しました。"
-      redirect_to employees_supplier_external_staff_path(@supplier, @external_staff)
+      redirect_to employees_external_staff_url
     else
       respond_to do |format|
         format.js
@@ -19,9 +19,11 @@ class Employees::ExternalStaffsController < ApplicationController
     end
   end
 
+  def index
+    @external_staffs = ExternalStaff.all
+  end
+
   def show
-    # login_idの先頭部分以外を定義
-    @login_id_body = @external_staff.login_id_body
   end
 
   def edit
@@ -30,7 +32,7 @@ class Employees::ExternalStaffsController < ApplicationController
   def update
     if @external_staff.update(external_staff_params)
       flash[:success] = "外部Staff情報を更新しました。"
-      redirect_to employees_supplier_external_staff_path(@supplier, @external_staff)
+      redirect_to employees_external_staff_url
     else
       respond_to do |format|
         format.js
@@ -40,19 +42,19 @@ class Employees::ExternalStaffsController < ApplicationController
 
   def destroy
     @external_staff.destroy ? flash[:success] = "外部Staffを削除しました。" : flash[:alert] = "外部Staffを削除できませんでした。"
-    redirect_to employees_supplier_path(@supplier)
+    redirect_to redirect_to employees_external_staff_url
   end
 
   private
     def external_staff_params
-      params.require(:external_staff).permit(:name, :kana, :login_id, :login_id_body, :phone, :email)
+      params.require(:external_staff).permit(:name, :kana, :login_id, :phone, :email, :supplier_id)
     end
 
     def set_external_staff
       @external_staff = ExternalStaff.find(params[:id])
     end
 
-    def set_supplier
-      @supplier = Supplier.find(params[:supplier_id])
+    def set_suppliers
+      @suppliers = Supplier.all
     end
 end
