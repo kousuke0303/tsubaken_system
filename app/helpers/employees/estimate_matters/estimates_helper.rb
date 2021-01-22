0501fc7ebd99f960113f49918844e6374b71bf0d
EC2 Default User <ec2-user@ip-172-31-27-24.ap-northeast-1.compute.internal>
2021-01-22 02:38:14 +0000

    fix/seed

--- app/helpers/employees/estimate_matters/estimates_helper.rb
+++ app/helpers/employees/estimate_matters/estimates_helper.rb
@@ -1,2 +1,7 @@
 module Employees::EstimateMatters::EstimatesHelper
+  
+  def estimate_type(number)
+    type = ["A", "B", "C", "D", "E", "F", "G", "H"]
+    return type[number]
+  end
 end
--- db/seeds/estimate_matter.rb
+++ db/seeds/estimate_matter.rb
@@ -1,78 +1,60 @@
-SeedEstimateMatter1 = EstimateMatter.create!(title: "見積案件1",
-                                             content: "見積案件1の内容",
-                                             postal_code: "5940088",
-                                             prefecture_code: "神奈川県",
-                                             address_city: "テスト市テスト町",
-                                             address_street: "1-1-1",
-                                             client_id: 1,
-                                             )
-SeedEstimateMatter1.update!(created_at: Date.current.prev_month)
-SeedEstimateMatter1.sales_statuses.create!(status: "not_set", conducted_on: Date.current)
-
-SeedEstimateMatter2 = EstimateMatter.create!(title: "見積案件2",
-                                             content: "見積案件2の内容",
-                                             postal_code: "5940088",
-                                             prefecture_code: "神奈川県",
-                                             address_city: "テスト市テスト町",
-                                             address_street: "1-1-1",
-                                             client_id: 2,
-                                             )
-                                             
-SeedEstimateMatter2.sales_statuses.create!(status: "not_set", conducted_on: Date.current)                                             
+# 営業案件
+Client.all.each.with_index(1) do |client, index|
+  new_est = EstimateMatter.create!(title: "#{client.name} 邸",
+                           content: "屋根・外壁塗装工事",
+                           postal_code: client.postal_code,
+                           prefecture_code: client.prefecture_code,
+                           address_city: client.address_city,
+                           address_street: client.address_street,
+                           client_id: index,
+                           created_at: Date.today - rand(1..24).month
+                         )
+  EstimateMatterStaff.create!(estimate_matter_id: new_est.id, staff_id: rand(1..3))
+  EstimateMatterExternalStaff.create!(estimate_matter_id: new_est.id, external_staff_id: rand(1..3))
+end
 
 puts "CREATE! ESTIMATE_MATTER"
 
-EstimateMatterStaff.create!(estimate_matter_id: SeedEstimateMatter1.id, staff_id: 1)
-EstimateMatterStaff.create!(estimate_matter_id: SeedEstimateMatter2.id, staff_id: 2)
 
-puts "CREATE! ESTIMATE_MATTER_STAFF"
 
-EstimateMatterExternalStaff.create!(estimate_matter_id: SeedEstimateMatter1.id, external_staff_id: 1)
-EstimateMatterExternalStaff.create!(estimate_matter_id: SeedEstimateMatter2.id, external_staff_id: 2)
+# 営業案件ステータス
+EstimateMatter.all.each do |est|
+  new_sales_status = est.sales_statuses.create!(status: rand(14),
+                                                conducted_on: est.created_at + 10.days
+                                               )
+                            
+  SalesStatusEditor.create!(authority: "manager",
+                             member_id: rand(1..3),
+                             sales_status_id: new_sales_status.id)
+  SalesStatusMember.create!(authority: "staff",
+                             member_id: est.staffs.first.id,
+                             sales_status_id: new_sales_status.id)
+end
 
-puts "CREATE! ESTIMATE_MATTER_EXTERNAL_STAFF"
-
-SeedMatter1 = Matter.create!(title: "案件1",
-                             content: "案件1の内容",
-                             status: 0,
-                             estimate_matter_id: SeedEstimateMatter1.id
-                             )
-
-SeedMatter2 = Matter.create!(title: "案件2",
-                             content: "案件2の内容",
-                             status: 0,
-                             estimate_matter_id: SeedEstimateMatter2.id
-                             )
+# 営業案件からの案件作成
+EstimateMatter.where(id: 1..25).each do |est|
+ Matter.create!(title: est.title,
+                content: est.content,
+                status: 0,
+                estimate_matter_id: est.id
+               )
+end
 puts "CREATE! MATTER"
-
-MatterStaff.create!(matter_id: SeedMatter1.id, staff_id: 1)
-MatterStaff.create!(matter_id: SeedMatter2.id, staff_id: 2)
-
-puts "CREATE! MATTER_STAFF"
-
-MatterExternalStaff.create!(matter_id: SeedMatter1.id, external_staff_id: 1)
-MatterExternalStaff.create!(matter_id: SeedMatter2.id, external_staff_id: 2)
-
-puts "CREATE! MATTER_EXTERNAL_STAFF"
 
 # -----------------------------------------------------
       # TASK
 # -----------------------------------------------------
 
 3.times do |n|
-  Task.create!(title: "TASK#{ n + 1 }", status: 0, title: "DEFALTE_TASK_#{ n + 1 }",  content: "テスト内容", sort_order: n, default_task_id: "#{ n + 1 }")
+  Task.create!(status: 0, title: "DEFALTE_TASK_#{ n + 1 }", content: "テスト内容", sort_order: n, default_task_id: "#{ n + 1 }")
 end
 
 puts "CREATE! DEFALTE_TASK"
 
-3.times do |n|
-  Task.create!(title: "TASK#{ n + 1 }", status: 1, title: "当該タスク#{ n + 1 }", content: "テスト内容", sort_order: n, estimate_matter_id: SeedEstimateMatter1.id)
-  Task.create!(title: "TASK#{ n + 1 }", status: 2, title: "進行中タスク#{ n + 1 }", content: "テスト内容", sort_order: n, estimate_matter_id: SeedEstimateMatter2.id)
-end
 
-3.times do |n|
-  Task.create!(title: "TASK#{ n + 1 }", status: 1, title: "当該タスク#{ n + 1 }", content: "テスト内容", sort_order: n, matter_id: SeedMatter2.id)
-  Task.create!(title: "TASK#{ n + 1 }", status: 2, title: "進行中タスク#{ n + 1 }", content: "テスト内容", sort_order: n, matter_id: SeedMatter2.id)
+Matter.all.each_with_index do |matter, index|
+  Task.create!(status: 1, title: "当該タスク#{ index + 1 }", content: "テスト内容", sort_order: index, matter_id: matter.id)
+  Task.create!(status: 2, title: "進行中タスク#{ index + 1 }", content: "テスト内容", sort_order: index, matter_id: matter.id)
 end
 
 puts "CREATE! TASK"
