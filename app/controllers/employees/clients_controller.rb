ab6954d7be422e134e3e24bf1562bc14fe63317b
kousuke0303 <kousukeee0303@gmail.com>
2021-02-21 15:55:40 +0900

    fix/client_id_confirmed

--- app/controllers/employees/clients_controller.rb
+++ app/controllers/employees/clients_controller.rb
@@ -53,7 +53,7 @@ class Employees::ClientsController < ApplicationController
 
   private
     def client_params
-      params.require(:client).permit(:name, :kana, :gender, :login_id, :phone_1, :phone_2, :email, :birthed_on, :postal_code, :prefecture_code, :address_city, :address_street)
+      params.require(:client).permit(:name, :kana, :gender, :login_id, :confirmed, :phone_1, :phone_2, :email, :birthed_on, :postal_code, :prefecture_code, :address_city, :address_street)
     end
 
     def set_client
