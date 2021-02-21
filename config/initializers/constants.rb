25fe00de33076c21e2e1f73f52fe44fe83b64674
kousuke0303 <kousuke0303@gmail.com>
2021-02-21 11:30:52 +0900

    fix

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,5 @@
 module Constants
+  UPPERCASE_LATTER = ("A".."Z").to_a
   EMPLOYEES_TYPE_HASH = { "Manager": 1, "Staff": 2, "外部Staff": 3 }
   MONTH_HASH = { "1月": "01", "2月": "02", "3月": "03", "4月": "04", "5月": "05", "6月": "06",
                  "7月": "07", "8月": "08", "9月": "09", "10月": "10", "11月": "11", "12月": "12" }
