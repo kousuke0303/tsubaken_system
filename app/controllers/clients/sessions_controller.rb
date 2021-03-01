45418527d6cb62493e6b515c248968a1c43f6837
kousuke0303 <kousukeee0303@gmail.com>
2021-03-01 17:28:57 +0900

    fix

--- app/controllers/clients/sessions_controller.rb
+++ app/controllers/clients/sessions_controller.rb
@@ -1,19 +1,6 @@
 # frozen_string_literal: true
 
 class Clients::SessionsController < Devise::SessionsController
-  def create
-    self.resource = warden.authenticate!(auth_options)
-    if self.resource.confirmed
-      set_flash_message!(:notice, :signed_in)
-      sign_in(resource_name, resource)
-      yield resource if block_given?
-      respond_with resource, location: after_sign_in_path_for(resource)
-    else
-      flash[:alert] = "ログインに失敗しました。"
-      self.resource = nil
-      redirect_to root_path
-    end
-  end
 
   def failed
     # warden で出力されたエラーを保存する
