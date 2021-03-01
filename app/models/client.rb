c99a4cbfefe17f7e00cf75ec3736fed76a43c425
kousuke0303 <kousukeee0303@gmail.com>
2021-03-01 17:28:22 +0900

    fix

--- app/models/client.rb
+++ app/models/client.rb
@@ -33,6 +33,16 @@ class Client < ApplicationRecord
     errors.add(:login_id, "は「CL-」から始めてください") if login_id.present? && !login_id.start_with?("CL-")
   end
 
+    # emailでなくlogin_idを認証キーにする
+    def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      if login_id = conditions.delete(:login_id)
+        where(conditions).where(login_id: login_id, confirmed: true).first
+      else
+        where(conditions).first
+      end
+    end
+
   # 登録時にemailを不要にする
   def email_required?
     false
