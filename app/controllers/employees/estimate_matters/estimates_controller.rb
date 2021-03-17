d1c8045119a146379da36934af8f7dddf7e9234d
kousuke0303 <kousukeee0303@gmail.com>
2021-03-17 09:24:54 +0900

    fix

--- app/controllers/employees/estimate_matters/estimates_controller.rb
+++ app/controllers/employees/estimate_matters/estimates_controller.rb
@@ -27,7 +27,7 @@ class Employees::EstimateMatters::EstimatesController < Employees::EstimateMatte
     @estimate = @estimate_matter.estimates.new(estimate_params)
     if @estimate.save
       # 送られてきたデフォルトカテゴリを、見積の持つカテゴリとしてコピー
-      if @after_category_arrey.present?
+      if @after_category_array.present?
         params_categories = params[:estimate][:category_ids].split(",").map(&:to_i)
         params_categories.each.with_index(1) do |params_id, i|
           default_category = Category.find(params_id)
@@ -59,22 +59,13 @@ class Employees::EstimateMatters::EstimatesController < Employees::EstimateMatte
   def update
     if @estimate.update(estimate_params)
       # 送られてきたデフォルトカテゴリを、見積の持つカテゴリとしてコピー
-      if @after_category_arrey.present?
-        # 差分比較
-        comparison_for_category
-        
-        # ①パターン：カテゴリ初登録及びカテゴリ増加
-        if @add_category_arrey != "nil"
-          register_categories(@add_categories)
-        end
-        
-        # ②パターン：カテゴリ削除
-        if @delete_category_arrey != "nil"
-          decrease_category(@delete_categories)
-        end
-        
-        # 順番変更
-        change_category_order
+      if @after_category_array.present?        
+        comparison_for_category # 差分比較        
+        # ①パターン：カテゴリ初登録及びカテゴリ増加        
+        register_categories(@add_categories) if @add_category_array != "nil"        
+        # ②パターン：カテゴリ削除      
+        decrease_category(@delete_categories) if @delete_category_array != "nil"                
+        change_category_order # 順番変更
       end
       @response = "success"
       @estimate.calc_total_price
@@ -128,39 +119,33 @@ class Employees::EstimateMatters::EstimatesController < Employees::EstimateMatte
     def refactor_params_category_ids
       params_categories = params[:estimate][:category_ids].split(",").map(&:to_i)
       # 空欄を除く
-      @after_category_arrey = []
-      params_categories.each do |params_categopry|
-        unless params_categopry == 0
-          @after_category_arrey << params_categopry
-        end
+      @after_category_array = []
+      params_categories.each do |params_categopry|      
+        @after_category_array << params_categopry unless params_categopry == 0
       end
     end
     
     # 差分調査
     def comparison_for_category
-      before_category_arrey = @estimate.estimate_details.pluck(:category_id)
+      before_category_array = @estimate.estimate_details.pluck(:category_id)
       # カテゴリが増えている場合
-      if (@after_category_arrey - before_category_arrey) == [nil]
-        @add_category_arrey = "nil"
-      elsif @after_category_arrey == before_category_arrey
-        @add_category_arrey = "nil"
+      if (@after_category_array - before_category_array) == [nil] || @after_category_array == before_category_array
+        @add_category_array = "nil"
       else
-        @add_categories = @after_category_arrey - before_category_arrey
+        @add_categories = @after_category_array - before_category_array
       end
       # カテゴリが減っている場合
-      if (before_category_arrey - @after_category_arrey) == [nil]
-        @delete_category_arrey = "nil"
-      elsif before_category_arrey == @after_category_arrey
-        @delete_category_arrey = "nil"
+      if (before_category_array - @after_category_array) == [nil] || before_category_array == @after_category_array
+        @delete_category_array = "nil" 
       else
-        @delete_categories = before_category_arrey - @after_category_arrey
+        @delete_categories = before_category_array - @after_category_array
       end
     end
     
     # カテゴリ登録
-    def register_categories(category_id_arrey)
+    def register_categories(category_id_array)
       before_detail_count = @estimate.estimate_details.count
-      category_id_arrey.each.with_index(1) do |category_id, index|
+      category_id_array.each.with_index(1) do |category_id, index|
         default_category = Category.find(category_id)
         @estimate.estimate_details.create(category_name: default_category.name,
                                           category_id: default_category.id,
@@ -169,15 +154,15 @@ class Employees::EstimateMatters::EstimatesController < Employees::EstimateMatte
     end
     
     # カテゴリ減少
-    def decrease_category(category_id_arrey)
-      category_id_arrey.each do |category_id|
+    def decrease_category(category_id_array)
+      category_id_array.each do |category_id|
         @estimate.estimate_details.where(category_id: category_id).destroy_all
       end
     end
     
     # 順番変更
     def change_category_order
-      details = @estimate.estimate_details.sort_by{|detail| @after_category_arrey.index(detail.category_id)}
+      details = @estimate.estimate_details.sort_by{|detail| @after_category_array.index(detail.category_id)}
       details.each.with_index(1) do |detail, i|
         detail.update(sort_number: i * 100)
       end
