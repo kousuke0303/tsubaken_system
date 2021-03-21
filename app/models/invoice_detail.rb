9dcc420f0ac5048b9ca69e6272244bf238d187ab
kousuke0303 <kousuke0303@gmail.com>
2021-03-21 20:54:54 +0900

    fix

--- app/models/invoice_detail.rb
+++ app/models/invoice_detail.rb
@@ -11,11 +11,6 @@ class InvoiceDetail < ApplicationRecord
   validates :amount, allow_blank: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 99999999 }
   validates :note, length: { maximum: 100 }
   
-  with_options on: :object_update do
-    validates :unit, presence: true
-    validates :amount, presence: true
-  end
-  
   # 単価と数量から合計金額を保存
   def calc_total
     price.present? && amount.present? ? self.total = price * amount : self.total = 0
