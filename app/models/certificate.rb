4166f9cda6a532531c970b27f4b4e65bf6029db4
kousuke0303 <kousuke0303@gmail.com>
2021-04-07 09:41:26 +0900

    fix

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -3,7 +3,6 @@ class Certificate < ApplicationRecord
   belongs_to :message, optional: true
   belongs_to :estimate_matter, optional: true
   
-  # 診断書(certificate)が作成されるたびに、positionカラムにシーケンシャルな数字が自動で追加される
   acts_as_list scope: :estimate_matter
   
   validates :title, presence: true, length: { maximum: 30 }
