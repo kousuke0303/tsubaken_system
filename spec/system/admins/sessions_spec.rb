5f0975c04de96123f65255f8d83f1b1e08817ab9
kousuke0303 <kousuke0303@gmail.com>
2021-04-26 11:00:12 +0900

    fix

--- spec/system/admins/sessions_spec.rb
+++ spec/system/admins/sessions_spec.rb
@@ -1,15 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe "管理者ログイン機能", type: :system do
-  before do
-    @admin = Admin.first
+  let(:admin) { Admin.first }
+
+  context "ログイン" do
+    context "ログインID・パスワードが適性" do
+      it "ログインが出来る" do
+        visit root_url
+        find(".to-login-btn").click
+        fill_in "login_id", with: admin.login_id
+        fill_in "password", with: "password"
+        find("#login-submit").send_keys(:enter)
+        expect(current_path).to eq admins_top_path
+      end
+    end
+
+    context "ログインIDが不正" do
+      it "ログインが出来ない" do
+        visit root_url
+        find(".to-login-btn").click
+        fill_in "login_id", with: "AD-miss"
+        fill_in "password", with: "password"
+        find("#login-submit").send_keys(:enter)
+        expect(page).to have_content("ログインに失敗しました")
+      end
+    end
+
+    context "パスワードが不正" do
+      it "ログインが出来ない" do
+        visit root_url
+        find(".to-login-btn").click
+        fill_in "login_id", with: admin.login_id
+        fill_in "password", with: "passmiss"
+        find("#login-submit").send_keys(:enter)
+        expect(page).to have_content("ログインに失敗しました")
+      end
+    end
   end
-  it "ログインが出来る" do
-    visit root_url
-    find(".to-login-btn").click
-    fill_in "login_id", with: @admin.login_id
-    fill_in "password", with: "password"
-    find("#login-submit").send_keys(:enter)
-    expect(current_path).to eq admins_top_path
+
+  context "ログアウト" do
   end
 end
