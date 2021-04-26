88ca89c24e7d885774355be8adf7cd121c9493bc
kousuke0303 <kousuke0303@gmail.com>
2021-04-27 00:03:46 +0900

    fix/rspec

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,20 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  before do
-    @admin = Admin.first
-  end
+  let(:admin) { Admin.first }
 
   # validates :name, presence: true, length: { maximum: 30 }
   context "name" do
     it "存在しなければ無効" do
-      @admin.name = ""
-      expect(@admin.valid?).to eq(false)
+      admin.name = ""
+      expect(admin.valid?).to eq(false)
     end
 
     it "31文字以上は無効" do
-      @admin.name = (1..31).map{ "a" }.join
-      expect(@admin.valid?).to eq(false)
+      admin.name = (1..31).map{ "a" }.join
+      expect(admin.valid?).to eq(false)
     end
   end
 
@@ -22,59 +20,59 @@ RSpec.describe Admin, type: :model do
   # validate :admin_login_id_is_correct?
   context "login_id" do
     it "存在しない場合無効" do
-      @admin.login_id = ""
-      expect(@admin.valid?).to eq(false)
+      admin.login_id = ""
+      expect(admin.valid?).to eq(false)
     end
 
     it "8文字未満は無効" do
-      @admin.login_id = "AD-ad"
-      expect(@admin.valid?).to eq(false)
+      admin.login_id = "AD-ad"
+      expect(admin.valid?).to eq(false)
     end
 
     it "13文字以上は無効" do
-      @admin.login_id = "AD-" + (1..10).map{ "a" }.join
-      expect(@admin.valid?).to eq(false)
+      admin.login_id = "AD-" + (1..10).map{ "a" }.join
+      expect(admin.valid?).to eq(false)
     end
 
     it "AD-から始まっていなければ無効" do
-      @admin.login_id = "A-admintest"
-      expect(@admin.valid?).to eq(false)
+      admin.login_id = "A-admintest"
+      expect(admin.valid?).to eq(false)
     end
   end
 
   # validates :phone, format: { with: VALID_PHONE_REGEX }, allow_blank: true
   context "phone" do
     it "10桁未満は無効" do
-      @admin.phone = "090111111"
-      expect(@admin.valid?).to eq(false)
+      admin.phone = "090111111"
+      expect(admin.valid?).to eq(false)
     end
 
     it "12桁の異常は無効" do
-      @admin.phone = "090111122223"
-      expect(@admin.valid?).to eq(false)
+      admin.phone = "090111122223"
+      expect(admin.valid?).to eq(false)
     end
 
     it "0から始まっていなければは無効" do
-      @admin.phone = "1111111111"
-      expect(@admin.valid?).to eq(false)
+      admin.phone = "1111111111"
+      expect(admin.valid?).to eq(false)
     end
 
     it "数字以外の値は無効" do
-      @admin.phone = "aaaaaaaaaa"
-      expect(@admin.valid?).to eq(false)
+      admin.phone = "aaaaaaaaaa"
+      expect(admin.valid?).to eq(false)
     end
   end
 
   # validates :email, length: { maximum: 254 }, allow_blank: true
   context "email" do
     it "255文字以上は無効" do
-      @admin.email = (1..245).map{ "a" }.join + "@email.com"
-      expect(@admin.valid?).to eq(false)
+      admin.email = (1..245).map{ "a" }.join + "@email.com"
+      expect(admin.valid?).to eq(false)
     end
 
     it "emailに「@」が含まれない場合無効" do
-      @admin.email = "test_formattest.com"
-      expect(@admin.valid?).to eq(false)
+      admin.email = "test_formattest.com"
+      expect(admin.valid?).to eq(false)
     end
   end
 end
--- spec/models/staff_spec.rb
+++ spec/models/staff_spec.rb
@@ -1,131 +1,85 @@
 require 'rails_helper'
 
 RSpec.describe Staff, type: :model do
-  before do
-    @department = create(:department)
-    @staff = create(:staff, department: @department)
+  let(:staff) { Staff.first }
+  
+  # validates :name, presence: true, length: { maximum: 30 }
+  context "name" do
+    it "存在しなければ無効" do
+      staff.name = ""
+      expect(staff.valid?).to eq(false)
+    end
+
+    it "31文字以上は無効" do
+      staff.name = (1..31).map{ "a" }.join
+      expect(staff.valid?).to eq(false)
+    end
   end
 
-  describe "name、login_id、phone、emailのバリデーション確認" do
-    # validates :name, presence: true, length: { maximum: 30 }
-    it "nameが存在し、なおかつ30文字以内の時、有効" do
-      expect(@staff.valid?).to eq(true)
-    end
-    
-    # validates :name, presence: true, length: { maximum: 30 }
-    it "nameが存在し、なおかつ30文字以上の時、無効" do
-      @staff.name = "スタッフテスト、スタッフテスト、スタッフテスト、スタッフテスト"
-      expect(@staff.valid?).to eq(false)
-    end
-    
-    # validates :login_id, presence: true, length: { in: 8..12 }
-    it "login_idが存在しない時、無効" do
-      @staff.login_id = ""
-      expect(@staff.valid?).to eq(false)
-    end
-    
-    # validates :login_id, presence: true, length: { in: 8..12 }
-    it "login_idが存在し、なおかつ8文字未満の時、無効" do
-      @staff.login_id = "ST-s-1"
-      expect(@staff.valid?).to eq(false)
-    end
-    
-    # validates :login_id, presence: true, length: { in: 8..12 }
-    it "login_idが存在し、なおかつ12文字より多い時、無効" do
-      @staff.login_id = "ST-staff-1222"
-      expect(@staff.valid?).to eq(false)
-    end
-    
-    # validates :login_id, presence: true, length: { in: 8..12 }, uniqueness: true
-    it "login_idが一意でない時、無効" do
-      FactoryBot.create(:staff, login_id: "ST-staff-9", department: @department)
-      @staff.login_id = "ST-staff-9"
-      expect(@staff.valid?).to eq(false)
-    end
-    
-    # validates :login_id, presence: true, length: { in: 8..12 }
-    # validate :staff_login_id_is_correct?
-    it "login_idが存在しているが、STから始まっていない時、無効" do
-      @staff.login_id = "T-staff-9"
-      expect(@staff.valid?).to eq(false)
-    end
-    
-    # validates :phone, format: { with: VALID_PHONE_REGEX }, allow_blank: true
-    it "phoneが11桁の場合、有効" do
-      expect(@staff.valid?).to eq(true)
-    end
-    
-    # validates :phone, format: { with: VALID_PHONE_REGEX }, allow_blank: true
-    it "phoneが12桁の場合、無効" do
-      @staff.phone = "080111122223"
-      expect(@staff.valid?).to eq(false)
+  # validates :login_id, presence: true, length: { in: 8..12 }, uniqueness: true
+  # validate :staff_login_id_is_correct?
+  context "login_id" do
+    it "存在しない場合無効" do
+      staff.login_id = ""
+      expect(staff.valid?).to eq(false)
     end
 
-    # validates :email, length: { maximum: 254 }, format: { with: VALID_EMAIL_REGEX }, allow_blank: true
-    it "emailが254文字以内の場合、有効、emailに「@」が含まれる場合、有効" do
-      expect(@staff.valid?).to eq(true)
+    it "8文字未満は無効" do
+      staff.login_id = "ST-st"
+      expect(staff.valid?).to eq(false)
     end
 
-    # validates :email, length: { maximum: 254 }, allow_blank: true
-    it "emailが254文字より多い場合、無効" do
-      @staff.email = "test_length_test_length_test_length_test_length_test_length_test_length_test_length_test_length_test_length_test_length_test_length_test_length_test_length_test_length_test_length_test_length_test_length_test_length_test_length_test_length_test_length@email.com"
-      expect(@staff.valid?).to eq(false)
+    it "13文字以上は無効" do
+      staff.login_id = "ST-" + (1..10).map{ "a" }.join
+      expect(staff.valid?).to eq(false)
     end
 
-    # validates :email, format: { with: VALID_EMAIL_REGEX }, allow_blank: true
-    it "emailに「@」が含まれない場合、無効" do
-      @staff.email = "test_formattest.com"
-      expect(@staff.valid?).to eq(false)
+    it "ST-から始まっていなければ無効" do
+      staff.login_id = "M-stafftest"
+      expect(staff.valid?).to eq(false)
     end
-    
-    # validates :postal_code, format: { with: VALID_POSTAL_CODE_REGEX }, presence: true
-    it "郵便番号が存在し、なおかつハイフンなしの7桁で設定されていたら、有効" do
-      expect(@staff.valid?).to eq(true)
+  end
+
+  # validates :phone, format: { with: VALID_PHONE_REGEX }, allow_blank: true
+  context "phone" do
+    it "10桁未満は無効" do
+      staff.phone = "090111111"
+      expect(staff.valid?).to eq(false)
     end
-    
-    # validates :postal_code, format: { with: VALID_POSTAL_CODE_REGEX }, presence: true
-    it "郵便番号が存在しなければ、無効" do
-      @staff.postal_code = ""
-      expect(@staff.valid?).to eq(false)
+
+    it "12桁の異常は無効" do
+      staff.phone = "090111122223"
+      expect(staff.valid?).to eq(false)
     end
-    
-    # validates :postal_code, format: { with: VALID_POSTAL_CODE_REGEX }, presence: true
-    it "郵便番号が7桁でなければ、無効" do
-       @staff.postal_code = "11122"
-      expect(@staff.valid?).to eq(false)
+
+    it "0から始まっていなければは無効" do
+      staff.phone = "1111111111"
+      expect(staff.valid?).to eq(false)
     end
-    
-    # validates :prefecture_code, presence: true
-    it "都道府県が存在していたら、有効" do
-      expect(@staff.valid?).to eq(true)
+
+    it "数字以外の値は無効" do
+      staff.phone = "aaaaaaaaaa"
+      expect(staff.valid?).to eq(false)
     end
-    
-    # validates :prefecture_code, presence: true
-    it "都道府県が存在していなかったら、無効" do
-      @staff.prefecture_code = ""
-      expect(@staff.valid?).to eq(false)
+  end
+
+  # validates :email, length: { maximum: 254 }, allow_blank: true
+  context "email" do
+    it "255文字以上は無効" do
+      staff.email = (1..245).map{ "a" }.join + "@email.com"
+      expect(staff.valid?).to eq(false)
     end
-    
-    # validates :address_city, presence: true
-    it "市区町村が存在していたら、有効" do
-      expect(@staff.valid?).to eq(true)
+
+    it "emailに「@」が含まれない場合無効" do
+      staff.email = "test_formattest.com"
+      expect(staff.valid?).to eq(false)
     end
-    
-    # validates :address_city, presence: true
-    it "市区町村が存在していなかったら、無効" do
-      @staff.address_city = ""
-      expect(@staff.valid?).to eq(false)
-    end
-    
-    # validates :address_street, presence: true
-    it "町名番地が存在していたら、有効" do
-      expect(@staff.valid?).to eq(true)
-    end
-    
-    # validates :address_street, presence: true
-    it "町名番地が存在していなかったら、無効" do
-      @staff.address_street = ""
-      expect(@staff.valid?).to eq(false)
+  end
+
+  context "department_id" do
+    it "blankは無効" do
+      staff.department_id = nil
+      expect(staff.valid?).to eq(false)
     end
   end
 end
