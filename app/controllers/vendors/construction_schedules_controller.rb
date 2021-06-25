47305dfc8eef791dd4c79646f1f59e7045b69eba
kousuke0303 <kousuke0303@gmail.com>
2021-06-25 23:30:48 +0900

    fix/route

--- app/controllers/vendors/construction_schedules_controller.rb
+++ app/controllers/vendors/construction_schedules_controller.rb
@@ -1,2 +1,12 @@
 class Vendors::ConstructionSchedulesController < ApplicationController
+  before_action :set_construction_schedule, except: :index
+
+  def show
+    @construction_schedule_pictures = @construction_schedule.images
+  end
+
+  private
+    def set_construction_schedule
+      @construction_schedule = ConstructionSchedule.find(params[:id])
+    end
 end
