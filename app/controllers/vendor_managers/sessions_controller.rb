18da43ba3c47340ac3705a3228047c971a7703d2
kousuke0303 <kousuke0303@gmail.com>
2021-06-26 12:02:38 +0900

    remove/sup_man_devise_controllers

--- app/controllers/vendor_managers/sessions_controller.rb
+++ app/controllers/vendor_managers/sessions_controller.rb
@@ -1,27 +1,19 @@
-# frozen_string_literal: true
-
 class VendorManagers::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  def failed
+    # warden で出力されたエラーを保存する
+    flash[:alert] = "ログインに失敗しました。"
+    redirect_to root_url
+  end
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  protected
+    def auth_options
+    # 失敗時に recall に設定したパスのアクションが呼び出されるので変更
+    # { scope: resource_name, recall: "#{controller_path}#new" } # デフォルト
+      { scope: resource_name, recall: "#{controller_path}#failed" }
+    end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    # application_controllerのbefore_action回避
+    def use_before_action?
+      false
+    end
 end
